104152f022b58f5f9cf33cbddd20cf637e03c292
shifa-s11 <shifa22154074@akgec.ac.in>
2026-01-04 19:09:37 +0530

    Update CORS to allow deployed frontend

--- src/main.ts
+++ src/main.ts
@@ -12,9 +12,9 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173',
-      'https://webinar-client-blush.vercel.app/',
+      'https://webinar-client-blush.vercel.app',
       frontendUri,
-    ],
+    ].filter(Boolean),
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });

d3d75f3d490572415e38e1eb0488b29f9c59b658
shifa-s11 <shifa22154074@akgec.ac.in>
2026-01-04 17:17:24 +0530

    added frontend url

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,15 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  
+  app.enableCors({
+    origin: [
+      'http://localhost:5173',             
+      'https://your-frontend-domain.com',   
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
